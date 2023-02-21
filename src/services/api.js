257f124853f63a29c8686e70905c11b36ea3e034
Daniel Kawasaka <dkawasaka@gmail.com>
2023-02-21 05:12:06 -0300

    add env

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "https://api.esgtechpower.com/api/v1/",
+  baseURL: `${process.env.REACT_APP_URL_API}/api/v1/`,
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
